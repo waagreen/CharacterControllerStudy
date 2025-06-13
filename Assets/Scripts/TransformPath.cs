f23e04b4e9be1220c1a4cb47724e78902a92f970
Wagner Costa <silvacostawagner@gmail.com>
2025-06-13 13:06:30 -0300

    now is possible to platforms travel the reverse path

--- Assets/Scripts/TransformPath.cs
+++ Assets/Scripts/TransformPath.cs
@@ -1,37 +1,54 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 public class TransformPath : MonoBehaviour
 {
     [Min(1)][SerializeField] private int initialPoint = 1;
+    [SerializeField] private bool reverse = false;
+
     protected int waypointIndex;
+    protected int direction;
+    private List<Transform> points;
 
     private void OnValidate()
     {
         initialPoint = Mathf.Min(transform.childCount, initialPoint);
+        direction = reverse ? -1 : 1;
     }
 
     public void InitializePath()
     {
         OnValidate();
+
         waypointIndex = initialPoint - 1;
+
+        points = new();
+        foreach (Transform child in transform) points.Add(child);
     }
 
     public Vector3 GetCurrentPoint()
     {
-        return transform.GetChild(waypointIndex).position;
+        return points[waypointIndex].position;
     }
 
     public Vector3 GetNextPoint()
     {
-        waypointIndex += 1;
-        if (waypointIndex >= transform.childCount) waypointIndex = 0;
+        int next = waypointIndex + direction;
+        if ((next >= points.Count) || (next < 0))
+        {
+            direction *= -1;
+        }
+
+        waypointIndex += direction;
 
         return GetCurrentPoint();
     }
 
     private void OnDrawGizmosSelected()
     {
+        if (points == null) return;
+
         int order = 1;
         GUIStyle style = new()
         {
@@ -40,9 +57,10 @@ public class TransformPath : MonoBehaviour
             alignment = TextAnchor.MiddleCenter,
             richText = true
         };
-        
-        foreach (Transform child in transform)
+
+        foreach (Transform child in points)
         {
+            if (child == null) continue;
             Gizmos.color = (order != initialPoint) ? Color.magenta : Color.yellow;
             Gizmos.DrawSphere(child.position, 0.15f);
             Handles.Label(child.position + Vector3.up * 0.3f, order.ToString(), style);
